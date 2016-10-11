40d8db4f4777e8e2b871327aa54ec09f7939a438
cathera <yr-yang15@mails.tsinghua.edu.cn>
2016-10-11 20:31:16 +0800

    structure

--- src/communication.py
+++ src/communication.py
@@ -4,39 +4,30 @@ import socket
 import queue
 import select
 
-class recv_thread(threading.Thread):
+class IO_Thread(threading.Thread):
     def __init__(self):
-        self.request_list=[]
-        #Access gamemain instrction queue directly?
-        order_list_0=queue.Queue()
-        order_list_1=queue.Queue()
-        threading.Thread.__init__(self)
-    def run(self):
-        pass
-class send_thread(threading.Thread):
-    def __init__(self, request):
         self.send_queue=queue.Queue()
-        self.request=request
+        self.player_dict={}
         threading.Thread.__init__(self)
     def run(self):
-        pass
+        #Test code
+        #Test code
 
-class main_handler(socketserver.BaseRequestHandler):
+class Main_Handler(socketserver.BaseRequestHandler):
     def handler(self):
-        send=send_thread(request)
-        send.daemon=True
-        send.append(send)
-        recv.request_list.append(request)
-        send.start
+        message_queue=queue.Queue()
+        self.player_dict[message_queue]=self.request
 
-class main_tcp_server(socketserver.TCPServer):
+class Main_Tcp_Server(socketserver.TCPServer):
     def __init__(self, server_address, handler):
         self.send=[]
-        self.recv=recv_thread
+        self.io=IO_Thread()
+        self.io.start()
         socket.TCPServer.__init__(self, server_address, handler)
     def serialize(non_string_object):
         pass
     def send_message_to_players(non_string_object,player_no):
-        send[player_no].send_queue.put(serialize(non_string_object))
+        #self.send[player_no].send_queue.put(serialize(non_string_object))
+        pass
     def add_order_to_game_main():
         pass
