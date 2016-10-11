0cfa43f7486b4b20c27c208b99f80d7e0adf62f7
cathera <yr-yang15@mails.tsinghua.edu.cn>
2016-10-11 21:53:23 +0800

    Basic structure

--- src/communication.py
+++ src/communication.py
@@ -1,33 +1,50 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
 import threading
-import socketserver
 import socket
 import queue
-import select
+import asyncore
 
-class IO_Thread(threading.Thread):
-    def __init__(self):
-        self.send_queue=queue.Queue()
-        self.player_dict={}
-        threading.Thread.__init__(self)
-    def run(self):
-        #Test code
-        #Test code
 
-class Main_Handler(socketserver.BaseRequestHandler):
-    def handler(self):
-        message_queue=queue.Queue()
-        self.player_dict[message_queue]=self.request
+class IOHandler(asyncore.dispatcher):
 
-class Main_Tcp_Server(socketserver.TCPServer):
-    def __init__(self, server_address, handler):
-        self.send=[]
-        self.io=IO_Thread()
-        self.io.start()
-        socket.TCPServer.__init__(self, server_address, handler)
-    def serialize(non_string_object):
+    def __init__(self, sock):
+        asyncore.dispatcher.__init__(self, sock)
+        self.info_queue = queue.Queue()
+
+    def handle_read(self):
+        instruction = str(self.recv(8192))
+        if instruction:
+            self.add_to_gamemain(instruction.decode('utf-8'))
+
+    def handle_write(self):
+        self.send(self.info_queue.get().encode('utf-8'))
+
+    def writable(self):
+        return not self.info_queue.empty()
+
+    def add_to_gamemain(self, instruction):
         pass
-    def send_message_to_players(non_string_object,player_no):
-        #self.send[player_no].send_queue.put(serialize(non_string_object))
-        pass
-    def add_order_to_game_main():
+
+
+class MainServer(asyncore.dispatcher):
+
+    def __init__(self, host_addr):
+        asyncore.dispatcher.__init__(self)
+        self.conn_list = []
+        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.set_reuse_addr()
+        self.bind(host_addr)
+        self.listen(5)
+        print('Server initialized')
+
+    def handle_accept(self):
+        pair = self.accept()
+        print("Accept")
+        if pair is not None:
+            handler = IO_Handler(pair[0])
+            self.conn_list.append(handler)
+
+    def send_to_player(self, info):
+        # Put info into corresponding conn's queue
         pass
