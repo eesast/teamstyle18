81fdd1cff1e5b9b5c853f64298238b9818ab08df
bqb3927586 <250767048@qq.com>
2016-10-13 13:24:52 +0800

    删除了多于的GAMEMAIN

--- src/skills.py
+++ src/skills.py
@@ -2,87 +2,8 @@
 # -*- coding: utf-8 -*-
 
 import math
-import unit
-import gamemain
-
-class GameMain:
-    units = {}  # 单位dict
-    hqs = []  # 主基地
-    buildings = []  # 中立建筑
-    turn_flag = 0  # 谁的回合
-    turn_num = 0  # 回合数
-    phase_num = 0  # 回合阶段指示
-    buffs0 = []  # 选手0的buffs
-    buffs1 = []  # 选手1的buffs
-
-    def __init__(self):
-        pass
-
-    def windetermine_phase(self):
-        # 胜利判定
-        pass
-
-    def cleanup_phase(self, ai_id):
-        # 单位死亡判定
-        """
-
-        :type ai_id: int
-        """
-        pass
-
-    def skill_phase(selfself, ai_id):
-        # 技能结算
-        """
-
-        :type ai_id: int
-        """
-        pass
-
-    def move_phase(self, ai_id):
-        # 移动指令结算
-        """
-
-        :type ai_id: int
-        """
-        pass
-
-    def produce_phase(self, ai_id):
-        # 兵种获取指令结算
-        """
-
-        :type ai_id: int
-        """
-        pass
-
-    def capture_phase(self, ai_id):
-        # 占领建筑阶段
-        """
-
-        :type ai_id: int
-        """
-        pass
-
-    def next_tick(self):
-        # 进入下一回合前通信等任务
-        pass
-
-    def to_string(self):
-        # 将当前状态信息返回，用String,Json什么都行，你们自己起名字吧
-        pass
-
-
-
-def Get_id_information(id):
-    for k in A.units:
-        if (k == id):
-            return A.units[k]
-
-
-def Get_distance(my_position,enemy_position):
-    x = my_position[0] - enemy_position[0]
-    y = my_position[1] - enemy_position[1]
-    return (x * x + y * y) ** 0.5
-
+from src import unit
+from src import gamemain
 
 def receive_orders(order):
     for k in range(len(order)):
@@ -107,6 +28,15 @@ def receive_orders(order):
         elif(order[k][0]=='superman_skill2'):
             superman_skill2(order[k][1])
 
+def Get_id_information(id):
+    for k in A.units:
+        if (k == id):
+            return A.units[k]
+
+def Get_distance(my_position, enemy_position):
+    x = my_position[0] - enemy_position[0]
+    y = my_position[1] - enemy_position[1]
+    return (x * x + y * y) ** 0.5
 
 # 电子对抗坦克技能1
 def bolt_tank_skill1(id, attack_id):
